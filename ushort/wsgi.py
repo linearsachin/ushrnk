68fdfc795ec9ee7abed99d9ab41bed3ebecccb52
SacHin Yadav <sachineg39@gmail.com>
2020-08-23 05:23:43 +0530

    V1

--- ushort/wsgi.py
+++ ushort/wsgi.py
@@ -10,11 +10,12 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 import environ
 from django.core.wsgi import get_wsgi_application
-env = environ.Env()
-environ.Env.read_env()
+
 try:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',os.environ.get("DJANGO_SETTINGS_MODULE"))
 except:
+    env = environ.Env()
+    environ.Env.read_env()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',env("DJANGO_SETTINGS_MODULE"))
 
 application = get_wsgi_application()
