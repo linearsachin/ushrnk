433068df46e1cfb4b622ed75c4fba749681493ea
SacHin Yadav <sachineg39@gmail.com>
2020-08-23 08:04:51 +0530

    CLICKS

--- core/models.py
+++ core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 import os
-debug = __import__(os.environ.get('DJANGO_SETTINGS_MODULE'))
+# debug = __import__(os.environ.get('DJANGO_SETTINGS_MODULE'))
 class ShrnkUrl(models.Model):
     og_url = models.URLField()
     shrnk_url_slug = models.CharField(max_length=10)
@@ -8,10 +8,10 @@ class ShrnkUrl(models.Model):
     def __str__(self):
         return self.shrnk_url_slug
     def get_shrnk_url(self):
-        if debug.development.DEBUG:
-            return f"http://127.0.0.1:8000/u/{self.shrnk_url_slug}"
-        else:
-            return f"https://ushrnk.herokuapp.com/u/{self.shrnk_url_slug}"
+        # if debug.development.DEBUG:
+        #     return f"http://127.0.0.1:8000/u/{self.shrnk_url_slug}"
+        # else:
+        return f"https://ushrnk.herokuapp.com/u/{self.shrnk_url_slug}"
 
     class Meta:
         db_table = ''
